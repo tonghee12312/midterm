271d8bda46c4c773cbf088c84586f4b60120f1eb
Forge-15 <Forge-15@DESKTOP-USIT37L>
2023-02-22 02:24:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/finalyearproject/Redemption.kt
@@ -0,0 +1,11 @@
+package com.example.finalyearproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Redemption : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redemption)
+    }
+}
---
+++ app/src/main/java/com/example/finalyearproject/RedemptionDetails.kt
@@ -0,0 +1,11 @@
+package com.example.finalyearproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RedemptionDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redemption_details)
+    }
+}
